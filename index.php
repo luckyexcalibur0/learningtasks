db0d3a9cacf865e90a23e4d67b7bc4879cc52c57
luckyexcalibur0 <99784342+luckyexcalibur0@users.noreply.github.com>
2022-04-07 22:08:12 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<! DOCTYPE html>
+<html>
+ <head>
+   <title> My Home Page </title>
+   
+  </head>
+  <body>
+    <h1>Welcome to my website! The names' Daniel Delos Santos! </h1>
+    
+    <ul>
+      <li> <a href="index.php"> Home </a> </li>
+      <li> <a href="/LT2/index.html"> Learning Task 2 </a> </li>
+    
+  </body>
+ 
+  
+  
+</html>
